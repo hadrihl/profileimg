aacef464de8d15bd0a972f478fb57ad74711da39
hadrihl <hadrihilmi@gmail.com>
2023-06-20 21:23:33 +0800

    initial commit.

---
+++ src/main/java/com/example/profileimg/ProfileimgApplication.java
@@ -0,0 +1,13 @@
+package com.example.profileimg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileimgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileimgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/profileimg/ProfileimgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profileimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileimgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
